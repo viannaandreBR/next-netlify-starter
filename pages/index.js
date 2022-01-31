0c8ad4dc8e84bff7dcf205e074458176f9874f4a
Andre <andre@bioinformaticalabs.com>
2022-01-31 13:18:15 -0300

    from Github
    from Github

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my app! from GitHub" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
